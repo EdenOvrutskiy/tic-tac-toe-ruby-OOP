6327c3d07caeddddfc6f461e970400aa1222cc06
Eden O <-->
2022-07-31 17:19:26 -0400

    used ruby's built-in Struct instead of my own class for data-structure encapsulation

--- tic_tac_toe_OOP.rb
+++ tic_tac_toe_OOP.rb
@@ -10,9 +10,6 @@ def print_welcome_messages
 end
 
 ##OOP:
-##I want an interface to the board data:
-#instead of my program writing board_hash{:top_left}
-#it should do board.top_left
 
 board_data =
   [
@@ -24,67 +21,12 @@ board_data =
 #used 9 variables and name them accordingly, but this lets
 #me practice encapsulating data structures
 
-class Board
-  #enables API access to board_data
-  #before: board_data[0][1]
-  #after: board_data.top_middle
-  attr_accessor :top_left, :top_middle, :top_right,
-                :middle_left, :middle_middle, :middle_right,
-                :bottom_left, :bottom_middle, :bottom_right
-  
-  def initialize(board_data)
-    @top_left      = board_data[0][0]
-    @top_middle    = board_data[0][1]
-    @top_right     = board_data[0][2]
-    @middle_left   = board_data[1][0]
-    @middle_middle = board_data[1][1]
-    @middle_right  = board_data[1][2]
-    @bottom_left   = board_data[2][0]
-    @bottom_middle = board_data[2][1]
-    @bottom_right  = board_data[2][2]
-  end
-end
+#implementing Struct class instead of creating a class.
 
+Board = Struct.new(:top_left, :top_middle, :top_right,
+                   :middle_left, :middle_middle, :middle_right,
+                    :bottom_left, :bottom_middle, :bottom_right)
+#turn array entries into input arguments
+my_board = Board.new(*board_data.flatten)
 
-###########
-##old attempts to create objects / classes, keeping it below
-##to later reference
-class Board_old
-  attr_reader :top_left, :top_middle, :top_right
-  attr_reader :middle_left, :middle_middle, :middle_right
-  attr_reader :bottom_left, :bottom_middle, :bottom_right
-
-  def initialize
-    @top_left = nil
-    @top_middle = nil
-    @top_right = nil
-    @middle_left = nil
-    @middle_middle = nil
-    @middle_right = nil
-    @bottom_left = nil
-    @bottom_middle = nil
-    @bottom_right = nil
-  end
-end
-
-class Board_displayer
-  attr_reader :board
-  
-  def initialize(board)
-    @board = board
-  end
-  
-  def display
-    puts board.top_left
-  end
-end
-
-class User_input_reader
-  def initialize
-    @input = gets
-  end
-end
-######################
-
-
-p Board.new(board_data).top_left
+p my_board.top_left
